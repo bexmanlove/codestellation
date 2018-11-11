5522f093262da30a3c3ab94687003c9c2df721fa
Sarah Manlove <smanlove29@gmail.com>
2018-11-11 06:10:38 -0500

    add from android studio

--- app/src/main/java/com/example/sarahmanlove/codestellation/Main4Activity.java
+++ app/src/main/java/com/example/sarahmanlove/codestellation/Main4Activity.java
@@ -1,17 +1,30 @@
 package com.example.sarahmanlove.codestellation;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class Main4Activity extends AppCompatActivity {
+
+    private static final int REQUEST_CALL=1;
+    private String phoneNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main4);
+        setContentView(R.layout.activity_main);
 
         Button button = findViewById(R.id.btnRestart);
         button.setOnClickListener(new View.OnClickListener() {
@@ -19,12 +32,15 @@ public class Main4Activity extends AppCompatActivity {
                 startActivity(new Intent(Main4Activity.this, MainActivity.class));
             }
         });
-
-        Button btn = findViewById(R.id.btnHelpline);
-        btn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                startActivity(new Intent(Main4Activity.this, MainActivity.class));
-            }
-        });
+    }
+    public void BtnSetEmergency_onClick(View view) {
+        String number = "8888269438";
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel:" + number));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        startActivity(intent);
     }
 }
+
